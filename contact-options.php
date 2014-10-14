0af251271a76f7e05234e6e13888ae5022ce0e2c
Javier Prieto <jprieton@gmail.com>
2014-10-14 17:00:58 -0430

    Inicial

---
+++ contact-options.php
@@ -0,0 +1,24 @@
+<div class="wrap">
+		<h2>JP Theme Tools - Contacto</h2>
+		<?php if (isset($_GET['settings-updated'])) { ?>
+	    <div id="message" class="updated">
+	        <p><strong><?php _e('Settings saved.') ?></strong></p>
+	    </div>
+		<?php } ?>
+		<form method="post" action="options.php">
+				<?php
+				settings_fields('jptt-settings-group');
+				do_settings_sections('jptt-settings-group');
+				?>
+				<table class="form-table">
+						<tr valign="top">
+								<th scope="row">Email</th>
+								<td>
+										<input type="text" class="regular-text" name="contact-form-email" value="<?php echo esc_attr(get_option('contact-form-email')); ?>" />
+										<p class="description">Email de destino del formulario de contacto.</p>
+								</td>
+						</tr>
+				</table>
+				<?php submit_button(); ?>
+		</form>
+</div>
---
+++ functions/common-functions.php
@@ -0,0 +1,41 @@
+<?php
+
+if (!function_exists('debug')) {
+
+	/**
+	 * Dumps information about the variable wrapped in &lt;pre&gt; tag and, optionally, terminate the current script.
+	 *
+	 * @param mixed $var The variable you want to dump.
+	 * @param bool $stop If true terminate the current script.
+	 * @return void
+	 */
+	function debug($var, $stop = false)
+	{
+		echo '<pre>';
+		var_dump($var);
+		echo '</pre>';
+
+		if ($stop) {
+			die();
+		}
+	}
+
+}
+
+function jptt_get_template($template_file)
+{
+	if ($overridden_template = locate_template('theme-tools/' . $template_file)) {
+		// locate_template() returns path to file
+		// if either the child theme or the parent theme have overridden the template
+		load_template($overridden_template);
+	} else {
+		// If neither the child nor parent theme have overridden the template,
+		// we load the template from the 'templates' sub-directory of the directory this file is in
+		load_template(JPTT_PLUGIN_PATH . '/templates/' . $template_file);
+	}
+}
+
+function jptt_valid_url($url)
+{
+	return esc_url_raw($url, array('http', 'https'));
+}
