cc6f3bad4b21d9ecd575b78d4dc15c6c0c8e5df6
Your Name <you@example.com>
2015-02-16 04:23:14 -0430

    Open Graph metas

--- social/filters.php
+++ social/filters.php
@@ -2,21 +2,29 @@
 
 add_action('wp_head', function() {
 
-	if (is_page())
-	{
+	$meta = array();
+	$meta['og:type'] = 'website';
+	$meta['og:site_name'] = get_bloginfo('name');
+
+	if (is_page() or is_singular()) {
 		$current_post = get_post();
 		$meta['og:title'] = esc_attr($current_post->post_title);
 		$meta['og:description'] = esc_attr($current_post->post_excerpt);
-		$meta['og:site_name'] = get_bloginfo('name');
 		$meta['og:url'] = get_permalink($current_post->ID);
 		$meta['og:image'] = wp_get_attachment_url(get_post_thumbnail_id($current_post->ID));
+		if (!is_front_page()) {
+			$meta['og:type'] = 'article';
+			$meta['article:published_time'] = mysql2date('c', $current_post->post_date);
+			$meta['article:modified_time'] = mysql2date('c', $current_post->post_modified);
+		}
+	}
 
-		foreach ($meta as $key => $value) {
-			if (!empty($value))
-			{
-				printf('<meta name="%s" content="%s" />', $key, $value);
-				echo "\n";
-			}
+	$meta = (array) apply_filters('og_meta_tags', $meta);
+
+	foreach ($meta as $key => $value) {
+		if (!empty($value)) {
+			printf('<meta name="%s" content="%s" />', $key, $value);
+			echo "\n";
 		}
 	}
 }, 1);
