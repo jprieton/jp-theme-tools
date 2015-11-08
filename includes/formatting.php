a0dad66d1b1f23b9a98d48b26ea48cb91e0aeb34
jprieton <jprieton@gmail.com>
2015-11-08 04:34:14 -0430

    function is_url()

--- includes/formatting.php
+++ includes/formatting.php
@@ -5,7 +5,7 @@ if ( !function_exists( 'is_url' ) ) {
 	/**
 	 * Verifies that an url is valid.
 	 * @since 0.12.3
-	 * @param string $url Email address to verify.
+	 * @param string $url Url address to verify.
 	 * @return string|bool Either false or the valid url address.
 	 */
 	function is_url( $url ) {
