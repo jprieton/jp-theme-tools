3141e88f0d1493d6d1f3c91314e03596adc1380a
Javier Prieto <jprieton@gmail.com>
2016-03-30 16:41:27 -0430

    Bootstrap helper

--- includes/init.php
+++ includes/init.php
@@ -2,20 +2,24 @@
 
 /** Constants */
 define( 'JPTT_BASEPATH', realpath( __DIR__ . '/..' ) );
-define( 'JPTT_SYSTEM', realpath( JPTT_BASEPATH . DIRECTORY_SEPARATOR . 'includes' ) );
-define( 'JPTT_LIBRARIES', realpath( JPTT_BASEPATH . DIRECTORY_SEPARATOR . 'libraries' ) );
+define( 'JPTT_SYSTEM', realpath( JPTT_BASEPATH . '/includes' ) );
+define( 'JPTT_LIBRARIES', realpath( JPTT_BASEPATH . '/libraries' ) );
 define( 'JPTT_TEXTDOMAIN', 'jptt' );
 
 /** Required files */
 require_once JPTT_SYSTEM . DIRECTORY_SEPARATOR . 'class-jptt.php';
 require_once JPTT_SYSTEM . DIRECTORY_SEPARATOR . 'class-config.php';
-require_once JPTT_SYSTEM . DIRECTORY_SEPARATOR . 'class-input.php';
+//require_once JPTT_SYSTEM . DIRECTORY_SEPARATOR . 'class-input.php';
 require_once JPTT_SYSTEM . DIRECTORY_SEPARATOR . 'class-jptt_instagram.php';
+//require_once JPTT_SYSTEM . DIRECTORY_SEPARATOR . 'class-post-stats.php';
 
-if ( is_admin() ) {
+if ( is_admin() && !class_exists( 'Parsedown' ) ) {
 	/** Required libraries */
 	require_once JPTT_BASEPATH . '/libraries/parsedown.php';
 }
+
+/** Helpers */
+require realpath( JPTT_BASEPATH . '/helpers/bootstrap.php' );
 
 global $jptt;
 $jptt = new \jptt\jptt();
@@ -27,7 +31,7 @@ add_action( 'plugins_loaded', function() {
 } );
 
 /** Widgets */
-require_once realpath( JPTT_BASEPATH . '/widgets/bs-sidebar-search.php' ) ;
-require_once realpath( JPTT_BASEPATH . '/widgets/bs-navbar-menu.php' ) ;
-require_once realpath( JPTT_BASEPATH . '/widgets/bs-navbar-search.php' ) ;
-require_once realpath( JPTT_BASEPATH . '/widgets/bs-navbar-subscribe.php' ) ;
+require_once realpath( JPTT_BASEPATH . '/widgets/bs-sidebar-search.php' );
+require_once realpath( JPTT_BASEPATH . '/widgets/bs-navbar-menu.php' );
+require_once realpath( JPTT_BASEPATH . '/widgets/bs-navbar-search.php' );
+require_once realpath( JPTT_BASEPATH . '/widgets/bs-navbar-subscribe.php' );
