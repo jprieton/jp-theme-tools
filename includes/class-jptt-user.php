8b075aef929632f0b22a507404118d9b574c0f83
Your Name <you@example.com>
2016-04-10 23:00:26 -0430

    * JPTT_User

--- includes/class-jptt-user.php
+++ includes/class-jptt-user.php
@@ -42,7 +42,7 @@ class JPTT_User {
 	 * @param string $user_pass
 	 * @return int|WP_Error The newly created user's ID or a WP_Error object if the user could not be created.
 	 */
-	public function quick_register( $user_email, $user_pass ) {
+	public function user_register( $user_email, $user_pass ) {
 
 		$user_email = strtolower( trim( $user_email ) );
 
@@ -68,11 +68,29 @@ class JPTT_User {
 		return $user_id;
 	}
 
+	/**
+	 * 
+	 * @param string $user_email
+	 * @param string $user_pass
+	 * @param bool $remember
+	 * @return WP_User|WP_Error WP_User on success, WP_Error on failure.
+	 */
+	public function user_login_by_email( $user_email, $user_pass, $remember = null ) {
+
+		$user_email = strtolower( trim( $user_email ) );
+		$user_pass = trim( $user_pass );
+
+		$credentials = compact( 'user_login', 'user_password', 'remember' );
+		$user = wp_signon( $credentials, false );
+
+		return $user;
+	}
+
 }
 
-add_action( 'wp_ajax_nopriv_user_quick_register', function() {
+add_action( 'wp_ajax_nopriv_jptt_user_register', function() {
 	$nonce = filter_input( INPUT_POST, '_wpnonce', FILTER_SANITIZE_STRING );
-	$verify_nonce = (bool) wp_verify_nonce( $nonce, 'user_quick_register' );
+	$verify_nonce = (bool) wp_verify_nonce( $nonce, 'jptt_user_register' );
 
 	if ( !$verify_nonce ) {
 		$error = new WP_Error( 'action_disabled', __( 'Action disabled' ) );
@@ -88,20 +106,51 @@ add_action( 'wp_ajax_nopriv_user_quick_register', function() {
 		wp_send_json_error( $error );
 	}
 
-	do_action( 'pre_user_quick_register', $user_email, $user_pass );
+	do_action( 'pre_jptt_user_register', $user_email, $user_pass );
 
 	$user = JPTT_User::get_instance();
-	$user_id = $user->quick_register( $user_email, $user_pass );
+	$user_id = $user->user_register( $user_email, $user_pass );
 
-	do_action( 'post_user_quick_register', $user_id );
+	do_action( 'post_jptt_user_register', $user_id );
 
 	if ( is_wp_error( $user_id ) ) {
 		wp_send_json_error( $user_id );
 	} else {
 		add_user_meta( $user_id, 'show_admin_bar_front', 'false' );
-		$response[] = array(
+		$response = array(
 				'code' => 'register_success',
 				'message' => __( 'Register success' ),
+		);
+		wp_send_json_success( $response );
+	}
+} );
+
+add_action( 'wp_ajax_nopriv_jptt_user_login', function() {
+	$nonce = filter_input( INPUT_POST, '_wpnonce', FILTER_SANITIZE_STRING );
+	$verify_nonce = (bool) wp_verify_nonce( $nonce, 'user_login' );
+
+	if ( !$verify_nonce ) {
+		$error = new WP_Error( 'action_disabled', __( 'Action disabled' ) );
+		wp_send_json_error( $error );
+	}
+
+	$user_email = filter_input( INPUT_POST, 'user_email', FILTER_SANITIZE_EMAIL );
+	$user_pass = filter_input( INPUT_POST, 'user_password', FILTER_SANITIZE_STRING );
+	$remember = filter_input( INPUT_POST, 'user_password', FILTER_SANITIZE_STRING );
+
+	do_action( 'pre_user_user_login', $user_email, $user_pass );
+
+	$user = JPTT_User::get_instance();
+	$userdata = $user->user_login_by_email( $user_email, $user_pass, (bool) $remember );
+
+	do_action( 'post_user_user_login', $userdata );
+
+	if ( is_wp_error( $userdata ) ) {
+		wp_send_json_error( $userdata );
+	} else {
+		$response = array(
+				'code' => 'login_success',
+				'message' => __( 'Login success' ),
 		);
 		wp_send_json_success( $response );
 	}
