60dd298f9ccee0883b084619a1eafb4692986898
Your Name <you@example.com>
2016-04-18 22:35:18 -0430

    function get_favorite_posts()

--- jp-theme-tools.php
+++ jp-theme-tools.php
@@ -4,7 +4,7 @@
  * Plugin Name: JP WordPress Theme Tools
  * Plugin URI: https://github.com/jprieton/jp-theme-tools/
  * Description: Extends WordPress functionality for themes
- * Version: 0.18.1
+ * Version: 0.18.2
  * Author: Javier Prieto
  * Text Domain: jptt
  * Domain Path: /languages
