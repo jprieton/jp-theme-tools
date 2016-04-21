ccedeb43da8943872a875c7fff9f85428c150a8c
Javier Prieto <jprieton@gmail.com>
2016-04-21 14:06:33 -0430

    Pagination WooCommerce pagination Pagination filters

--- templates/global/pagination.php
+++ templates/global/pagination.php
@@ -7,9 +7,10 @@ if ( $wp_query->max_num_pages <= 1 ) {
 	return;
 }
 
-$class = (string) apply_filters('jptt_pagination_class', 'bs-pagination');
+$nav_class = (string) apply_filters('jptt_pagination_nav_class', 'bs-pagination');
+$ul_class = (string) apply_filters('jptt_pagination_ul_class', '');
 ?>
-<nav itemscope itemtype="http://schema.org/SiteNavigationElement" class="<?php echo $class ?>">
+<nav itemscope itemtype="http://schema.org/SiteNavigationElement" class="<?php echo $nav_class ?>">
 		<?php
 		$args = array(
 				'type' => 'list',
@@ -20,7 +21,7 @@ $class = (string) apply_filters('jptt_pagination_class', 'bs-pagination');
 				"<li><span class='page-numbers current'>"
 		);
 		$replace = array(
-				"<ul class='page-numbers pagination'>",
+				"<ul class='page-numbers pagination {$ul_class}'>",
 				"<li class='active'><span class='page-numbers current'>"
 		);
 		echo str_replace( $search, $replace, $paginate )
